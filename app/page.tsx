e8fc83d436d6b1c205ff59b490d5e8f73b6b31b6
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-06-12 12:33:30 +0700

    implement clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function Page() {
   return (
-    <Button size="lg" variant="custom">
-      Click here
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
