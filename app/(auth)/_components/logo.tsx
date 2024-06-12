7dcd7ee069d1b806f3434071d024380315248fc8
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-06-13 04:43:31 +0700

    implement dark mode

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+      <div className="bg-white p-1">
+        <Image src="/spooky.svg" alt="Game-X" height="80" width="80" />
+      </div>
+      <div className={cn("flex flex-col items-center", font.className)}>
+        <p className="text-xl font-semibold">N-Gaming</p>
+        <p className="text-sm text-muted-foreground">
+          Come Stream at Nolan&apos;s
+        </p>
+      </div>
+    </div>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { dark } from "@clerk/themes";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Game-X",
+  title: "N-Gaming",
   description: "FreeStreaming Platform for Gamers",
 };
 
@@ -16,9 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            forcedTheme="dark"
+            storageKey="N-gmaing-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
