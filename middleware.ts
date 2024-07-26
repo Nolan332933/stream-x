8f92fc3d501b48af3db648ffc0345f1b048198bb
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-07-26 22:53:44 +0700

    clerk user update

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,15 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/"]);
+const publicRoutes = ["/api/webhooks(.*)"];
+
+const isPublicRoute = (request: Request) => {
+  const url = new URL(request.url);
+  return publicRoutes.includes(url.pathname);
+};
 
 export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
+  if (isProtectedRoute(request) && !isPublicRoute(request)) {
     auth().protect();
   }
   return NextResponse.next();
