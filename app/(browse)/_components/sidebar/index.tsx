8852d2da4b1f31f618a601751748ccadb7ec9201
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-07-29 15:10:44 +0700

    implement sidebar

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,12 @@
+import { Toggle } from "./toggle";
+import { Wrapper } from "./wrapper";
+
+export const Sidebar = () => {
+  //fetch followed
+  //fetch recommended user
+  return (
+    <Wrapper>
+      <Toggle />
+    </Wrapper>
+  );
+};
