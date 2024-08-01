05b55f1ee2781e8166162af53c8aea1f8d18b43a
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-08-01 13:55:01 +0700

    Hydration error got fixed

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -8,6 +8,7 @@ import {
   ArrowRightFromLine,
 } from "lucide-react";
 import { Hint } from "@/components/hint";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Toggle = () => {
   const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
@@ -42,5 +43,14 @@ export const Toggle = () => {
         </div>
       )}
     </>
+  );
+};
+
+export const ToggleSkeleton = () => {
+  return (
+    <div className="p-3 pl-6 mb-2 hidden lg:flex items-center justify-between w-full">
+      <Skeleton className="h-6 w-[100px]" />
+      <Skeleton className="h-6 w-6" />
+    </div>
   );
 };
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,17 +1,32 @@
 "use client";
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+import { useState, useEffect } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
+import { useIsClient } from "usehooks-ts";
 
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  const isClient = useIsClient();
+
   const { collapsed } = useSidebar((state) => state);
+
+  if (!isClient)
+    return (
+      <aside className="fixed left-0 flex flex-col w-[70px]  h-full bg-background border-r border-[#2D2E35] z-50">
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
+
   return (
     <aside
       className={cn(
-        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+        "fixed left-0 flex flex-col w-60  h-full bg-background border-r border-[#2D2E35] z-50",
         collapsed && "w-[70px]"
       )}
     >
