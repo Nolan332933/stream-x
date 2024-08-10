5c340ebccb3da49cd71796f03bee9101d28cb715
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-08-10 21:32:53 +0700

    mobile admin sidebar fixed

--- app/(dashboard)/u/[username]/_components/sidebar/wrapper.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/wrapper.tsx
@@ -13,7 +13,7 @@ export const Wrapper = ({ children }: WrapperProps) => {
   return (
     <aside
       className={cn(
-        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+        "fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50",
         collapsed && "lg:w-[70px]"
       )}
     >
