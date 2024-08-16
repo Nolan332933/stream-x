a3fae65b1460c840a45fd7c953d376bc41aeefad
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-08-16 18:55:20 +0700

    implement public feed

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,7 +1,21 @@
+// export default function Page() {
+//   return (
+//     <div className="flex flex-col gap-y-4">
+//       <h1>Home</h1>
+//     </div>
+//   );
+// }
+
+import { Suspense } from "react";
+
+import { Results, ResultsSkeleton } from "./_components/results";
+
 export default function Page() {
   return (
-    <div className="flex flex-col gap-y-4">
-      <h1>Home</h1>
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
     </div>
   );
 }
